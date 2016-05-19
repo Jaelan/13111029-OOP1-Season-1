d6f5bb1ed238afb2eabf6e98821c91be84193626
jaelan sopari <jaelansopari@gmail.com>
2016-05-19 22:09:37 +0700

    Abstarak & Interface

---
+++ Abstrak&Interface/src/abstrak/pkginterface/AbstrakInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstrak.pkginterface;
+
+/**
+ *
+ * @author jay
+ */
+public abstract class AbstrakInterface {
+    
+    public void luaspersegipanjang (int pjng, int lebar){
+        int luas;
+                luas = pjng * lebar;
+    }
+    
+    public abstract void keliling (int pjng, int lebar);
+    
+    
+}
---
+++ Abstrak&Interface/src/abstrak/pkginterface/Anakabstrack.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstrak.pkginterface;
+
+/**
+ *
+ * @author jay
+ */
+
+public class Anakabstrack extends AbstrakInterface{
+
+    @Override
+    public void keliling(int pjng, int lebar) {
+       
+    }
+
+  
+}
